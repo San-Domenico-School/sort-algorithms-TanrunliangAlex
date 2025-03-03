1ee7e87b5ce761f602d60c28fa592fd8e43af9ea
TanrunliangAlex <143214650+TanrunliangAlex@users.noreply.github.com>
2025-03-03 12:32:04 -0800

    sb

--- Dealer.java
+++ Dealer.java
@@ -3,11 +3,11 @@ import java.util.ArrayList;
 
 /**
  * This class declares, instantiates, and initializes a new deck.  Students 
- * should then implement the sort method at the botton of this class to see 
- * if they understanding basic ArrayList sort algorithms.
+ * should then implement the sort method at the bottom of this class to see 
+ * if they understand basic ArrayList sort algorithms.
  * 
- * @author Bruce Gustin
- * @version Feb 8, 2023
+ * @author Alex Tan
+ * @version Feb 27, 2025
  */
 
 public class Dealer extends Actor
@@ -18,7 +18,7 @@ public class Dealer extends Actor
     private Card[] insertionSort;
     private Card[] mergeSort;
     
-    // Instantiates a new deck
+    
     public Dealer()
     {
         deck = new Deck();
@@ -26,7 +26,7 @@ public class Dealer extends Actor
         initializeSortArrays();
     }
     
-    // Adds cards to board after Deck is initialized
+    
     public void addedToWorld(World world)
     {
         showCards(world, shuffledDeck, 80);
@@ -34,10 +34,8 @@ public class Dealer extends Actor
         showCards(world, insertionSort(insertionSort, shuffledDeck.length), 360);
         showCards(world, mergeSort(mergeSort, shuffledDeck.length), 500);
     }    
-    // Shuffles the unshuffled deck
     
     
-    // Sets all the sorted arrays equal to the shuffledCard array
     private void initializeSortArrays()
     {
         selectionSort = new Card[shuffledDeck.length];
@@ -55,56 +53,121 @@ public class Dealer extends Actor
         }
     }
     
-    // Places card on board at y
+    
     private void showCards(World world, Card[] cards, int y)
     {
-        
        for(int i = 0; i < cards.length; i++)
        {
            world.addObject(cards[i],58 + 30 * i, y);
        } 
     }
     
-    /* This is where the students attempt their sort algorithm by taking the
-     * cards from the shuffled list and adding them to the sorted list.     
-    **/
     
-    /* In this space write the pseudocode for your selection sort
-     * 
-     * 
-     * 
-     */
     private Card[] selectionSort(Card[] arr, int n)
     {
-        //put sort algorithm here
         
-        return selectionSort;
+        for (int i = 0; i < n - 1; i++)
+        {
+            int minIndex = i; 
+            
+            
+            for (int j = i + 1; j < n; j++)
+            {
+                if (arr[j].getValue() < arr[minIndex].getValue())
+                {
+                    minIndex = j;
+                }
+            }
+            
+            
+            Card temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+        
+        return arr; 
     }
     
-    /* In this space write the pseudocode for your insertion sort
-     * 
-     * 
-     * 
-     */
+    
     private Card[] insertionSort(Card[] arr, int n)
     {
         
-        //put sort algorithm here
+        for (int i = 1; i < n; i++)
+        {
+            Card key = arr[i];  
+            int j = i - 1;
+            
+            while (j >= 0 && arr[j].getValue() > key.getValue())
+            {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            
+            arr[j + 1] = key;
+        }
         
-        return insertionSort;
+        return arr; 
     }
     
-    /* In this space write the pseudocode for your merge sort
-     * 
-     * 
-     * 
-     */
     private Card[] mergeSort(Card[] arr, int n)
-     {
+    {
         
-        //put sort algorithm here
+        if (n <= 1)
+        {
+            return arr;
+        }
         
-        return mergeSort;
+        
+        int mid = n / 2;
+        Card[] left = new Card[mid];
+        Card[] right = new Card[n - mid];
+        
+        
+        System.arraycopy(arr, 0, left, 0, mid);
+        System.arraycopy(arr, mid, right, 0, n - mid);
+        
+        
+        left = mergeSort(left, mid);
+        right = mergeSort(right, n - mid);
+        
+        
+        return merge(left, right);
+    }
+    
+    
+    private Card[] merge(Card[] left, Card[] right)
+    {
+        int leftSize = left.length;
+        int rightSize = right.length;
+        Card[] merged = new Card[leftSize + rightSize];
+        
+        int i = 0, j = 0, k = 0;
+        
+        
+        while (i < leftSize && j < rightSize)
+        {
+            if (left[i].getValue() <= right[j].getValue())
+            {
+                merged[k++] = left[i++];
+            }
+            else
+            {
+                merged[k++] = right[j++];
+            }
+        }
+        
+        
+        while (i < leftSize)
+        {
+            merged[k++] = left[i++];
+        }
+        
+        
+        while (j < rightSize)
+        {
+            merged[k++] = right[j++];
+        }
+        
+        return merged; 
     }
 }
-
